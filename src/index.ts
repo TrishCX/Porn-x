00994f7b38e3b9d41856e242d4dc48f627c9f8ac
TrishCX <thisismyn07@gmail.com>
2023-11-06 18:26:30 -0500

    Added imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-// export * from "./classes/index";
-// export * from "./interfaces/index";
-// export * from "./collection/index";
-// export * from "./videos/sources";
-import { getInformation } from "./functions/getInformation";
-
-getInformation({
-  query: "Alison tyler",
-}).then((response) => console.log(response));
+export * from "./classes/index";
+export * from "./interfaces/index";
+export * from "./collection/index";
+export * from "./videos/sources";
